e8626bbf23e25885ddae6d57501d041291e91983
Mattia Lancellotti <lancellotti.mattia2002@gmail.com>
2021-07-03 14:09:42 +0200

    Using getopt instead of self created methods.

--- args.c
+++ args.c
@@ -1,137 +1,134 @@
-#include "mem.h"
+#include <string.h>
+
 #include "args.h"
-#include "utils.h"
 
-//args_error_flags
-//...0 [ COMING_SOON_ARG] 0 [ UNKNOWN_ARG ] 0 [ PARAMETER_ERROR ] 0 [ PARAMETER_MISSING ] 
-//
-//args_flags
-//...0 [ version action ] 0 [ help action ] 0 [ print action ] 0 [ save action ]
-passwd_mod_t *handle_args(const char **argv, const int argc) {
-	size_t length = DEFAULT_PASSWD_SIZE, times = 1;
-	int char_not_admitted_flags = 0, args_error_flags = 0, args_flags = 0;
+/* 
+ * It splits the string passed to check which types of characters are not to be
+ * included during the generation of the password.
+ */
+static int get_flags(char* /*str*/);
 
-	for (int i=1; i<argc; i++)
-		if (argv[i][0] == '-') {
-			if (strlen(argv[i]+1) > 1) {
-				if (!strcmp(argv[i]+1, "length")) {
-					if ((argc-1) == i)
-						args_error_flags|=PARAMETER_MISSING;
-					else if (check_num(argv[++i]))
-						length = (size_t)atoi(argv[i]);
-					else
-						args_error_flags|=PARAMETER_ERROR;
-				} else if (!strcmp(argv[i]+1, "not-admitted")) {
-					if ((argc-1) == i)
-						args_error_flags|=PARAMETER_MISSING;
-					else if (argv[++i][0] != '-') {
-					      char_not_admitted_flags = get_flags(argv[i]);
-					      if (char_not_admitted_flags&16)
-					      		args_error_flags|=PARAMETER_ERROR;
-					} else
-						args_error_flags|=PARAMETER_ERROR;
-				} else if (!strcmp(argv[i]+1, "times")) {
-					if ((argc-1) == i)
-						args_error_flags|=PARAMETER_MISSING;
-					else if (check_num(argv[++i]))
-						times = atoi(argv[i]);
-					else
-						args_error_flags|=PARAMETER_ERROR;
-				} else if (!strcmp(argv[i]+1, "save"))
-					args_error_flags|=COMING_SOON_ARG;
-				else if (!strcmp(argv[i]+1, "use"))
-					args_error_flags|=COMING_SOON_ARG;
-				else if (!strcmp(argv[i]+1, "print"))
-					args_flags|=2;
-				else if (!strcmp(argv[i]+1, "help"))
-					args_flags|=4;
-				else if (!strcmp(argv[i]+1, "version"))
-					args_flags|=8;
-				else
-					args_error_flags|=UNKNOWN_ARG;
-			} else if (strlen(argv[i]+1) == 1) {
-				switch(argv[i][1]) {
-				case 'l':
-					if ((argc-1) == i)
-						args_error_flags|=PARAMETER_MISSING;
-					else if (check_num(argv[++i]))
-						length = (size_t)atoi(argv[i]);
-					else
-						args_error_flags|=PARAMETER_ERROR;
-					break;
-				case 'n':
-					if ((argc-1) == i)
-						args_error_flags|=PARAMETER_MISSING;
-					else if (argv[++i][0] != '-') {
-						char_not_admitted_flags = get_flags(argv[i]);
-						if (char_not_admitted_flags&16)
-							args_error_flags|=PARAMETER_ERROR;
-					} else
-						args_error_flags|=PARAMETER_ERROR;
-					break;
-				case 't':
-					if ((argc-1) == i)
-						args_error_flags|=PARAMETER_MISSING;
-					else if (check_num(argv[++i]))
-						times = atoi(argv[i]);
-					else
-						args_error_flags|=PARAMETER_ERROR;
-					break;
-				case 's':
-					args_error_flags|=COMING_SOON_ARG;
-					break;
-				case 'p':
-					args_flags|=2;
-					break;
-				case 'h':
-					args_flags|=4;
-					break;
-				case 'u':
-					args_error_flags|=COMING_SOON_ARG;
-					break;
-				case 'v':
-					args_flags|=8;
-					break;
-				default:
-					args_error_flags=UNKNOWN_ARG;
-				}
-			} else
-				args_error_flags|=UNKNOWN_ARG;
-		} else
-			args_error_flags|=UNKNOWN_ARG;
+#define SET_BIT(bitarr, bitpos) ((bitarr) |= (bitpos))
+int handle_args(const int argc, char **argv,
+      passwd_conf_t * const restrict config_file)
+{
+   size_t length = DEFAULT_PASSWD_SIZE, times = 1;
+   int option_index = 0, c, success = 0;
 
-	return mem_alloc_passwd_mod_t(length, times, char_not_admitted_flags, args_error_flags, args_flags);
+   /* options flags */
+   int fh = 0, fp = 0, fv = 0;
+
+   /*
+    * Declaring arguments.
+    * Since the help message or the version of the program are not really
+    * password related, they have been split from the main `executing-branch` of
+    * the program. Basically the struct defined below tells the `getopt_long` 
+    * to assign a value to ret[x] instead of evaluating the parameter in
+    * `handle_args`. This allows to write a shorter and easier code.
+    *
+    *    Read `man getopt.3`;
+    */
+   /*
+    * TODO
+    *    - Add a `list-profiles` to list profiles (default action)
+    *    - Add a `del-profile` to delete the chosen profile (Not sure)
+    *    - Add a `profile` to select a profile to generate that password
+    *    - Add a 'config' to load a different config file
+    *    - Remove `save` because profiles should do it
+    *    - Add `encryption` to decide which algorithm is to be used (Not sure)
+    *    - Rename `print` to `stats`
+    *    - Add `temp` to generate a temporary password for One Time Use
+    *      (means that I need to implement On Time Passwords)
+    */
+   const struct option options[] = {
+      {"not-admitted", required_argument, 0, 'n'},
+      {"length",       required_argument, 0, 'l'},
+      {"save",         required_argument, 0, 's'},
+      {"times",        required_argument, 0, 't'},
+      {"stats",        no_argument,      &fp, 3 },
+      {"help",         no_argument,      &fh, 1 },
+      {"version",      no_argument,      &fv, 2 },
+      {0,              0,                 0,  0 }
+   };
+
+   /* Parsing arguments using getopt_long(..), check man getopt.3 */
+   while (1) {
+      c = getopt_long(argc, argv, "l:n:t:s:", options, &option_index);
+      if (c == -1)
+         break;
+
+      switch (c) {
+      case 0:
+         /* Found but should not handle it */
+         break;
+      case '?':
+         /* Unsuccessfull matching */
+         return 1;
+      case 's':
+         config_file->saving_functionality = ( optarg[0] == 'y' ? 1 : 0 );
+         break;
+      case 'l':
+         /* Sets the length of the password */
+         if ((length = (size_t)atoi(optarg)) && length >= PASSWD_MIN_LENGTH 
+                                             && length <= PASSWD_MAX_LENGTH)
+            /* If not then leave the default option */
+            config_file->length = length;
+
+         break;
+      case 'n':
+         /* Uses `get_flags(..)` to get an unsigned flag of banned characters */
+	 config_file->char_not_admitted = get_flags(optarg);
+         break;
+      case 't':
+         /* Sets the length of the password */
+         if ((times = atoi(optarg)) && times <= PASSWD_MAX_TIMES)
+            /* If not then leave the default option */
+            config_file->times = times;
+         break;
+      default:
+         /* Exits unsuccessfully */
+         return -1;
+      }
+   }
+
+   /* Returns successfully */
+   SET_BIT(success, fp);
+   SET_BIT(success, fh);
+   SET_BIT(success, fv);
+
+   return success;
 }
 
-//check the input of -not-admitted
-unsigned get_flags(const char *str) {
-	int i=0, flags=0;  // 0 ->digit, 0->l_char, 0->u_char, 0->sign
-	char **arr_not_admitted = split(str, ',');
-	while(i<MAX_NOT_ADMITTED_STR_NUM && arr_not_admitted[i]) {
-		if (!strcmp(arr_not_admitted[i], "digit"))
-			flags|=8;
-		else if (!strcmp(arr_not_admitted[i], "l_char"))
-			flags|=4;
-		else if (!strcmp(arr_not_admitted[i], "u_char"))
-			flags|=2;
-		else if (!strcmp(arr_not_admitted[i], "sign"))
-			flags|=1;
-		else {
-			flags|=16;
-			break;
-		}
-		i++;
-	}
+#define MAX_NOT_ADMITTED_STR_NUM 4
+static int get_flags(char *str)
+{
+   size_t i=0, flags=0;  // 0 ->digit, 0->l_char, 0->u_char, 0->sign
 
-	mem_dealloc_str_matrix(arr_not_admitted, i-1);
-	return flags;
-}			
+   char *token = NULL;
 
-#define PRINT_E(x, y) if (args_error_flags&x)\
-				printf("%s["#x"]%s: "y".\n", RED, RESET);
-void print_args_error(const size_t args_error_flags) {
-	PRINT_E(UNKNOWN_ARG, "unknown argument detected")
-	PRINT_E(PARAMETER_MISSING, "this argument requires a parameter")
-	PRINT_E(PARAMETER_ERROR, "this argument requires a different kind of parameter")
-	PRINT_E(COMING_SOON_ARG, "this argument is not defined yet");
+   /* 
+    * The strtok_r function creates a token every times it loops through the
+    * string.
+    */
+   while ((token = strtok_r(str, ",", &str)) && i < MAX_NOT_ADMITTED_STR_NUM) {
+      if (!strcmp(token, "digit"))
+	 SET_BIT(flags, 8);
+      else if (!strcmp(token, "l_char"))
+	 SET_BIT(flags, 4);
+      else if (!strcmp(token, "u_char"))
+	 SET_BIT(flags, 2);
+      else if (!strcmp(token, "sign"))
+	 SET_BIT(flags, 1);
+      else
+         /* 
+          * Returning 0 so we don't have to worry about checking stuff, it just
+          * acts as nothing happened.
+          */
+         return 0;
+
+      /* Counting tokens */
+      i++;
+   }
+
+   return flags;
 }
--- args.h
+++ args.h
@@ -1,18 +1,28 @@
 #ifndef ARGS_H
 #define ARGS_H
 
-#include "main.h" 
+#include <getopt.h>
 
-//ARGS_ERROR
-#define UNKNOWN_ARG       1
-#define PARAMETER_MISSING 2
-#define PARAMETER_ERROR   4
-#define COMING_SOON_ARG   8
+#include "main.h"
 
-enum TYPE { DIGIT, U_CHAR, L_CHAR, SIGN, NONE };
+#define PASSWD_MIN_LENGTH 4
+#define PASSWD_MAX_LENGTH 20
+#define PASSWD_MAX_TIMES  10
 
-passwd_mod_t *handle_args(const char **, const int);
-void print_args_error(const size_t);
-unsigned get_flags(const char *);
+/* 
+ * Used to handle the program's arguments.
+ *
+ * Don't mind the restricted pointer on `config_file`. Just a way to avoid that
+ * multiple functions modify it. I don't think it's going to happen since the
+ * program it's not even mutlithreaded.
+ *
+ * It also returns 1 if it detects an unknown option; while it returns 0 on
+ * every successfull execution.
+ *
+ * Also `config_file` should be defined with default options in mind. This way
+ * `handle_args` can avoid predicting bad things.
+ */
+int handle_args(const int argc, char** /*argv*/, 
+                  passwd_conf_t * const restrict /*config_file*/);
 
 #endif
