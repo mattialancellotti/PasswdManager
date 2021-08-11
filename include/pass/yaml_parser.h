4de98b85da46cc1420198f3f6662e800ef85e6cc
pixelunknown <lancellottimattia@gmail.com>
2021-08-11 16:59:23 +0200

    Some usefull macros

--- include/pass/yaml_parser.h
+++ include/pass/yaml_parser.h
@@ -6,8 +6,10 @@
 #if defined(__debug__)
 # define report(msg); \
     printf("%s [%d]: In func. %s, \"%s\"\n", __FILE__, __LINE__, __func__, msg);
+# define call(func);  func;
 #else
 # define report(msg);
+# define call(func);
 #endif
 
 const config_t *parse_settings(const char* /*file*/);
