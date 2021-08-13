4e349b5e0e270220d090cd7ff026b4471043a353
pixelunknown <lancellottimattia@gmail.com>
2021-08-13 12:40:44 +0200

    Hash and cryptographic functions

--- src/crypto.c
+++ src/crypto.c
@@ -31,10 +31,41 @@ char *hash_password(const char *password)
 
 int check_hash(const char *hash, const char *passwd)
 {
+   /* TODO:
+    *    - Need to check hash's length (>= crypto_pwhash_STRBYTES)
+    */
    just_exit(hash, NULL, -1);
    just_exit(passwd, NULL, -1);
 
    /* Checking the hash */
    int err = crypto_pwhash_str_verify(hash, passwd, strlen(passwd));
    return err;
+}
+
+int encrypt_content(const unsigned char *content, const char *passwd)
+{
+   /* Checking if the content is not empty */
+   just_exit(content, NULL, -1);
+
+   /* Defining parameters */
+   size_t clen = strlen((const char *)content);
+   size_t plen = strlen(passwd);
+
+   unsigned char nonce[crypto_aead_chacha20poly1305_NPUBBYTES];
+   unsigned char *key = malloc(sizeof(unsigned char) *
+                                 crypto_aead_chacha20poly1305_KEYBYTES);
+   unsigned char ciphertext[clen + crypto_aead_chacha20poly1305_ABYTES];
+   unsigned long long ciphertext_len;
+
+   /* Initiating encryption */
+   crypto_aead_chacha20poly1305_keygen(key);
+   randombytes_buf(nonce, sizeof(nonce));
+   key = memcpy(key, passwd, plen);
+
+   /* Encrypting the message */
+   int chacha20out =
+      crypto_aead_chacha20poly1305_encrypt(ciphertext, &ciphertext_len,
+                                             content, clen, NULL, 0, NULL,
+                                             nonce, key);
+   return chacha20out;
 }
