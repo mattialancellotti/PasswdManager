dd48998d8110be51996615b2081984f67d3e6b1b
pixelunknown <lancellottimattia@gmail.com>
2021-08-13 16:01:00 +0200

    new functions added, new structure to rapresent files

--- include/pass/os.h
+++ include/pass/os.h
@@ -1,6 +1,12 @@
 #ifndef OS_H
 #define OS_H
 
+/* This struct rapresent a file */
+typedef struct {
+   char *file_content;
+   int fd;
+} file_t;
+
 /*
  * This function searches for the $HOME directory of the user calling the
  * program. If not found a NULL is returned
@@ -8,5 +14,8 @@
  * The caller needs to free the returned pointer.
  */
 char *users_path(void);
+
+file_t *os_fopen_rw(const char* /*f_name*/);
+int os_fclose(file_t* /*file*/);
 
 #endif
