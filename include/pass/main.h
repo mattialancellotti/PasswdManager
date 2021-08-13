c105fa24abaa72ae0116e305a01833051f03d14f
pixelunknown <lancellottimattia@gmail.com>
2021-08-13 12:36:55 +0200

    Help message moved here, `passwd_t` structure moved to stats.h

--- include/pass/main.h
+++ include/pass/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <stddef.h>
 
 #define RED "\x1b[31m"
 #define YELLOW "\x1b[33m"
@@ -10,49 +9,21 @@
 
 #define DEFAULT_PASSWD_SIZE 8
 
-/* This is where the password is stored along with some information about it */
-/*
- * TODO
- *  This needs to be deleted/updated since it's not usefull anymore
+/* TODO:
+ *  - Rename to service_t (just more sense);
  */
 typedef struct {
-  char * restrict passwd;
-  
-  int consecutive_u_char, consecutive_l_char, consecutive_digit, consecutive_sign;
-  int repeated_u_char, repeated_l_char, repeated_digit, repeated_sign;
-  int number_digit, number_l_char, number_u_char, number_sign;
-} passwd_t;
-
-/*
- * TODO
- *  This struct needs to be updated with further information about the password
- *  and the profile structure like categories of characters or the default
- *  length for that password, the name of the service.
- */
-typedef struct {
-  char *name, desc;
-  size_t length;
-  int nchars;
-} passwd_profile_t;
-
-/* DEPRECATED */
-typedef struct {
-  char *saving_point;
-  size_t length, times;
-  int char_not_admitted,
-      saving_functionality;
+   size_t length, times;
+   unsigned char_not_admitted;
 } passwd_conf_t;
 
 /* This struct represent settings.yml */
 typedef struct {
-  char *passwds_file;
-  unsigned max_l, min_l;
-  unsigned accept_same_password,
-           colors, profiles;
+   char *passwds_file;
+   unsigned max_l, min_l;
+   unsigned accept_same_password,
+            colors, profiles;
 } config_t;
-
-/* Types of characters */
-enum TYPE { SIGN = 0, U_CHAR = 2, L_CHAR = 4, DIGIT = 8,  NONE = 0 };
 
 /*
  * This function creates a password of length `length`. Also  it accepts a
@@ -60,17 +31,14 @@ enum TYPE { SIGN = 0, U_CHAR = 2, L_CHAR = 4, DIGIT = 8,  NONE = 0 };
  */
 char *create_passwd(const size_t /*length*/, const int /*flags*/);
 
-/*
- * This one accepts the password and its length to execute some controls on it.
- * This is useful if you want to have some information about your password
- * like how many numbers are in it and things like that.
+/* 
+ * Just prints out the help message.
+ * It accepts an unsigned decimal that acts as a flag, if the second bit is 1
+ * it will also prints the version of the program.
  */
-void check_passwd(passwd_t ** const /*passwd_created*/, const size_t /*length*/);
-
-/*
- * This function prints out to the user the information gathered through the
- * `check_passwd` function.
+/* TODO:
+ *  - Maybe _Noreturn?
  */
-void print(const passwd_t * const /*passwd_info*/);
+void help(void);
 
 #endif
