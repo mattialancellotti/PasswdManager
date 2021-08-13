161a051f8deb917f4f4699c47ac4c57f6e1b47ff
pixelunknown <lancellottimattia@gmail.com>
2021-08-13 12:18:20 +0200

    Working on memory management for `passwd_conf_t`

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 /*
  * TODO:
  *    # Urgent
- *    - Include profiles
  *    - Include "password encryption/decryption"
  *
  *    # Project configuration
@@ -13,27 +12,10 @@
  *    
  *    # Features
  *    - Encrypts those passwords with sodium/libgcrypt.
- *    - Default action will be to ask a password and to print out the name of
- *      the profiles.
+ *    - Default action will be to print help message.
  *    - Save passwords in an XML encrypted file.
- *
- *    # Configuration file
- *    - settings.yaml
- *       login_pass_hashing :: [ md5, sha256, sha128 ]
- *       password_encryption :: [ .. ]
- *       passwds_file :: !!str `path`
- *       accept_same_passwd :: !!boolean `true|false`
- *       profiles_enabled :: !!boolean `true|false`
- *       colors :: !!boolean `true|false`
- *       passwd_min_len :: !!int `length`
- *       passwd_max_len :: !!int `length`
- *    - profiles.yaml
- *       length :: !!int `length`
- *       name :: !!str `name`
- *       desc :: !!str `description`
- *       nchars :: [ u_char, digit, l_char, sign ]
- *
  */
+#include <stdio.h>
 #include <unistd.h>
 #include <time.h>
 #include <ctype.h>
@@ -49,7 +31,6 @@
 
 #include <pass/defs.h>
 #include <pass/storage.h>
-#include <pass/utils.h>
 #include <pass/args.h>
 #if defined(__experimental__)
 #  include <pass/term.h>
@@ -58,8 +39,8 @@
 #endif
 
 /* TODO doc */
-static void mem_dealloc_passwd_t(passwd_t** /*passwd_s*/, const size_t /*length*/);
-static passwd_t* mem_alloc_passwd_t(void);
+static void mem_dealloc_passwd_t(passwd_conf_t** /*passwd_s*/);
+static passwd_conf_t* mem_alloc_passwd_t(void);
 
 /* Just the main function */
 int main(int argc, char **argv)
@@ -110,16 +91,11 @@ int main(int argc, char **argv)
     *
     * It has default options useful to the `handle_args` function.
     */
-   /*
-    * TODO
-    *    - Needs to be updated by following what happens in the options array
-    */
    passwd_conf_t config_file = { 
-      .saving_point = NULL,
       .length = DEFAULT_PASSWD_SIZE,
+      /* Temporarily disabled */
       .times  = 1,
       .char_not_admitted = 0,
-      .saving_functionality = 1
    };
 
 
@@ -139,11 +115,6 @@ int main(int argc, char **argv)
       return EXIT_SUCCESS;
    }
 
-   /* 
-    * TODO
-    *    Might want to extract this and put it into a function to create a more
-    *    flexible piece of code.
-    */
    /* Allocating the array of passwords */
    char *passwords[config_file.times];
 
@@ -166,6 +137,9 @@ int main(int argc, char **argv)
    return EXIT_SUCCESS;
 }
 
+/* TODO:
+ *  - Move this to its own file
+ */
 /* Really basic way of creating a password */
 char *create_passwd(const size_t length, const int flags)
 {
@@ -189,107 +163,35 @@ char *create_passwd(const size_t length, const int flags)
   return passwd;
 }
 
+/* Just the help function */
+void help()
+{
+   /* Pretty print */
+   printf("ezPass - Password Manager\n");
+   printf("\t-l, --length      : to specify the length of the password.\n"   );
+   printf("\t-n, --not-admitted: to specify which type of character is not\n");
+   printf("\t                    allowed [digit - u_char - l_char - sign]\n" );
+   printf("\t-t, --times       : to specify how many password you need\n"    );
+   printf("\t    --stats       : to print the stats\n"  );
+   printf("\t    --help        : to show this message\n");
+   printf("\t    --version     : to print the current version of the program\n");
+}
+
 /* Safely deallocates a password */
-static void mem_dealloc_passwd_t(passwd_t ** passwd_s, const size_t length)
+static void mem_dealloc_passwd_t(passwd_conf_t ** passwd_s)
 {
    /* Some controls to avoid segmatation faults */
    if (*passwd_s) {
-      if ((*passwd_s)->passwd) {
-         /* 
-          * Clears out the password to avoid it being stolen even after the
-          * deallocation.
-          */
-#if defined(__libsodium__)
-         sodium_memzero((*passwd_s)->passwd, length);
-#else
-	 memset((*passwd_s)->passwd, 0, length);
-	 free((*passwd_s)->passwd);
-#endif
-	 (*passwd_s)->passwd = NULL;
-      }
-
       free(*passwd_s);
       *passwd_s = NULL;
    }
 }
 
-static passwd_t *mem_alloc_passwd_t(void)
+/* TODO: Fix the problem with secure_malloc() */
+static passwd_conf_t *mem_alloc_passwd_t(void)
 {
-   /* Allocates and initialize the struct `passwd_t`, use sizeof(*new) understand */
-#if defined(__libsodium__)
-   passwd_t *new = sodium_malloc(sizeof(passwd_t));
-   sodium_memzero(new, 56);
-#else
-   passwd_t *new = malloc(sizeof(passwd_t));
-   memset(new, 0, 56);
-#endif
+   /* Allocates and initialize the struct `passwd_conf_t` */
+   //secure_malloc(passwd_conf_t *, new);
 
-   return new;
-}
-
-void check_passwd(passwd_t ** const passwd_created, const size_t length) {
-	if (!(*passwd_created) && !((*passwd_created)->passwd))
-		return;
-
-	enum TYPE last_c = NONE;
-	char already_used[length];
-	size_t arr_i=0, res=0;
-	char tmp;
-
-	for (size_t i=0; i<length; i++) {
-		if (!(res = search(already_used, arr_i, (tmp=(*passwd_created)->passwd[i]))))
-			already_used[++arr_i] = tmp;
-
-		if (isdigit(tmp)) {
-			if (last_c == DIGIT)
-				(*passwd_created)->consecutive_digit++;
-			else
-				last_c = DIGIT;
-
-			(*passwd_created)->number_digit++;
-
-			if (res)
-				(*passwd_created)->repeated_digit++;
-		} else if (islower(tmp)) {
-			if (last_c == L_CHAR)
-				(*passwd_created)->consecutive_l_char++;
-			else
-				last_c = L_CHAR;
-
-			(*passwd_created)->number_l_char++;
-
-			if (res)
-				(*passwd_created)->repeated_l_char++;
-		} else if (isupper(tmp)) {
-			if (last_c == U_CHAR)
-				(*passwd_created)->consecutive_u_char++;
-			else
-				last_c = U_CHAR;
-
-			(*passwd_created)->number_u_char++;
-
-			if (res)
-				(*passwd_created)->repeated_u_char++;
-		} else {
-			if (last_c == SIGN)
-				(*passwd_created)->consecutive_sign++;
-			else
-				last_c = SIGN;
-
-			(*passwd_created)->number_sign++;
-
-			if (res)
-				(*passwd_created)->repeated_sign++;
-		}
-	}
-}
-
-/* You really have to make this look better */
-void print(const passwd_t * const passwd_info) {
-	printf("\n%s===CONSECUTIVE CHARACTERS===%s\n", RED, RESET);
-	printf("\t| Consecutive upper case chars: %d\n\t| Consecutive lower case chars: %d\n\t| Consecutive digits: %d\n\t| Consecutive signs: %d\n", passwd_info->consecutive_u_char, passwd_info->consecutive_l_char, passwd_info->consecutive_digit, passwd_info->consecutive_sign);
-	printf("\n%s===REPEATED CHARACTERS===%s\n", RED, RESET);
-	printf("\t| Repeated upper case chars: %d\n\t| Repeated lower case chars: %d\n\t| Repeated digits: %d\n\t| Repeated signs: %d\n", passwd_info->repeated_u_char, passwd_info->repeated_l_char, passwd_info->repeated_digit, passwd_info->repeated_sign);
-	printf("\n%s===NUMBER OF EACH TYPE===%s\n", RED, RESET);
-	printf("\t| Number of upper case chars: %d\n\t| Number of lower case chars: %d\n\t| Number of digits: %d\n\t| Number of signs: %d\n", passwd_info->number_u_char, passwd_info->number_l_char, passwd_info->number_digit, passwd_info->number_sign);
+   return NULL;
 }
