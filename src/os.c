8a6572c52045270161bca2a1a1799c4c05d0bcda
pixelunknown <lancellottimattia@gmail.com>
2021-08-17 18:24:51 +0200

    New memory allocation function for the file_t struct

--- src/os.c
+++ src/os.c
@@ -13,6 +13,9 @@
 #include <pass/defs.h>
 #include <pass/os.h>
 
+static file_t *file_t_malloc(char* /*content*/, int /*fd*/);
+static void file_t_free(file_t* /*fstruct*/);
+
 char *users_path(void)
 {
    /* Getting user's uid to search its default $HOME directory */
@@ -28,13 +31,14 @@ char *users_path(void)
 
 file_t *os_fopen_rw(const char *f_name)
 {
+   /* Checking the file's name */
+   fatal_err(f_name, NULL, "File's name is null", NULL);
+
    /* Defining file infors like its descriptor and all the other information */
    struct stat f_infos;
    char *content = NULL;
+   file_t *ofile = NULL;
    int fd, st;
-
-   /* Checking the file's name */
-   fatal_err(f_name, NULL, "File's name is null", NULL);
 
    /* open() is a POSIX syscall. Take a look at open(2) in the man pages */
    /* TODO:
@@ -50,22 +54,28 @@ file_t *os_fopen_rw(const char *f_name)
    /* Checking if the file is new/empty */
    if (f_infos.st_size == 0) {
       /* Definin the new file */
-      file_t *ofile = malloc(sizeof(file_t));
-      ofile->file_content = content;
-      ofile->fd = fd;
+      ofile = file_t_malloc(content, fd);
 
       return ofile;
    }
 
    /* Mapping the file */
+   /* TODO:
+    *  - Close file descriptor before exiting;
+    */
    content = mmap(NULL, f_infos.st_size,
                   PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
    fatal_err(content, MAP_FAILED, "mmap", NULL);
 
    /* Definin the new file */
-   file_t *ofile = malloc(sizeof(file_t));
+   ofile = malloc(sizeof(file_t));
    ofile->file_content = content;
    ofile->fd = fd;
+
+   /*
+   exec_if((fd != -1), 
+         warn_user((close(fd) == -1), "Couldn't close the file"));
+         */
 
    /* Returning a pointer to the file */
    return ofile;
@@ -91,7 +101,7 @@ int os_fclose(file_t *file)
 
    /* Closing the file descriptor */
    int desc = close(file->fd);
-   fatal_err(desc, -1, "Couldn't close the file desc.", -1);
+   fatal_err(desc, -1, "close", -1);
 
    /* This is only needed for unmapping the file */
    if (file->file_content == NULL)
@@ -100,11 +110,11 @@ int os_fclose(file_t *file)
    /* Syincing the content of the file */
    size_t len = strlen(file->file_content);
    int sync = msync(file->file_content, len, MS_SYNC|MS_INVALIDATE);
-   fatal_err(sync, -1, "Couldn't sync the file", -1);
+   fatal_err(sync, -1, "msync", -1);
 
    /* Unmapping the file */
    int umap = munmap(file->file_content, len);
-   fatal_err(umap, -1, "Couldn't unmap the file", -1);
+   fatal_err(umap, -1, "munmap", -1);
 
 
    /* Freeing the structure */
@@ -113,4 +123,24 @@ close_exit:
 
    /* Returns successfully */
    return 0;
+}
+
+static file_t *file_t_malloc(char *content, int fd)
+{
+   /* Allocating memory for the new struct */
+   file_t *new_file = malloc(sizeof(file_t));
+   new_file->file_content = content;
+   new_file->fd = fd;
+
+   return new_file;
+}
+
+static void file_t_free(file_t *fstruct)
+{
+   exit_eq(fstruct, NULL,);
+
+   if (fstruct->file_content != NULL)
+      free(fstruct->file_content);
+
+   free(fstruct);
 }
